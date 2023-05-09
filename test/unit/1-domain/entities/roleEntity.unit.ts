ce92e451f7862cd43038cc891c0538bac170f5ac
Issac Cabral <issaccabrall@gmail.com>
2023-05-09 00:13:13 -0300

    test: add unit tests to role entity

---
+++ test/unit/1-domain/entities/roleEntity.unit.ts
@@ -0,0 +1,14 @@
+import { RoleEntity } from "@domain/entities/roleEntity";
+import { fakeRoleEntity } from "@test/utility/fakes/entities/roleEntity";
+
+describe("1-domain.entities.roleEntity", () => {
+  test("should be able to create a new role", () => {
+    const roleEntity = RoleEntity.create(fakeRoleEntity);
+
+    if (roleEntity.isRight()) {
+      const roleEntityExported = roleEntity.value.export();
+
+      expect(roleEntityExported.description).toBe(fakeRoleEntity.description);
+    }
+  });
+});
