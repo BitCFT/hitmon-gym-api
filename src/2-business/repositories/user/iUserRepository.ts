5e1a7b7795d1e9511a2262570f3f6b3f951e1c6d
Issac Cabral <issaccabrall@gmail.com>
2023-05-29 22:40:01 -0300

    chore: add type on create user repository method

--- src/2-business/repositories/user/iUserRepository.ts
+++ src/2-business/repositories/user/iUserRepository.ts
@@ -6,8 +6,15 @@ export type OutputFindById = IUserEntity | null;
 
 export type OutputFindByEmail = IUserEntity | null;
 
+export type InputCreateUser = {
+  email: string;
+  password: string;
+  userName: string;
+  accountVerificationCode: string;
+  accountVerificationCodeExpiresAt: Date;
+};
 export interface IUserRepository {
-  create(): Promise<IUserEntity>;
+  create(input: InputCreateUser): Promise<IUserEntity>;
   findById(id: string): Promise<OutputFindById>;
   findByEmail(id: string): Promise<OutputFindByEmail>;
 }
