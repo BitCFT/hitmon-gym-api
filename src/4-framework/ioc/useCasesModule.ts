af4562841fa963a5a999c979f649d1299f7a9102
Issac Cabral <issaccabrall@gmail.com>
2023-05-30 14:29:43 -0300

    chore: define useCases module

---
+++ src/4-framework/ioc/useCasesModule.ts
@@ -0,0 +1,6 @@
+import { CreateUserUseCase } from "@business/useCases/user/createUserUseCase";
+import { ContainerModule, interfaces } from "inversify";
+
+export const UseCasesModule = new ContainerModule((bind: interfaces.Bind) => {
+  bind(CreateUserUseCase).to(CreateUserUseCase);
+});
