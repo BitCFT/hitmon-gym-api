7fd98716b918e3eb50d689747edd2361e5574086
Issac Cabral <issaccabrall@gmail.com>
2023-06-07 18:52:24 -0300

    chore: remove unused methods

--- test/utility/mocks/service/loggerService.mock.ts
+++ test/utility/mocks/service/loggerService.mock.ts
@@ -1,13 +1,6 @@
 import { ILoggerService } from "@business/services/iLogger";
 
 export const logServiceMock: ILoggerService = {
-  log: jest.fn().mockResolvedValue(null) as unknown,
-  error: jest.fn().mockResolvedValue(null) as unknown,
-  warn: jest.fn().mockResolvedValue(null) as unknown,
-  info: jest.fn().mockResolvedValue(null) as unknown,
-  debug: jest.fn().mockResolvedValue(null) as unknown,
-  notice: jest.fn().mockResolvedValue(null) as unknown,
-  crit: jest.fn().mockResolvedValue(null) as unknown,
-  alert: jest.fn().mockResolvedValue(null) as unknown,
-  emerg: jest.fn().mockResolvedValue(null) as unknown,
+  log: jest.fn().mockResolvedValue(null),
+  error: jest.fn().mockResolvedValue(null),
 } as ILoggerService;
