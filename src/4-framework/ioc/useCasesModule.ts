bb6ca5cfa934fc277bf1d826c0cece58f3395287
Issac Cabral <issaccabrall@gmail.com>
2023-06-08 19:34:11 -0300

    test: should is not be able to resend code because exception in findByEmail method

--- src/4-framework/ioc/useCasesModule.ts
+++ src/4-framework/ioc/useCasesModule.ts
@@ -1,6 +1,8 @@
 import { CreateUserUseCase } from '@business/useCases/user/createUserUseCase';
+import { ResendAccountVerificationCodeUseCase } from '@business/useCases/user/resendAccountVerificationCodeUseCase';
 import { ContainerModule, interfaces } from 'inversify';
 
 export const UseCasesModule = new ContainerModule((bind: interfaces.Bind) => {
-  bind(CreateUserUseCase).to(CreateUserUseCase);
+  bind(CreateUserUseCase).toSelf();
+  bind(ResendAccountVerificationCodeUseCase).toSelf();
 });
---
+++ test/unit/2-business/useCases/user/resendAccountVerificationCodeUseCase.unit.ts
@@ -0,0 +1,32 @@
+import { container } from '@test/utility/ioc/inversifyConfigTests';
+import { ResendAccountVerificationCodeUseCase } from '@business/useCases/user/resendAccountVerificationCodeUseCase';
+import { InputResendAccountVerificationCodeDto } from '@business/dto/user/resendAccountVerificationCodeDto';
+import { userRepositoryMock } from '@test/utility/mocks/repository/userRepository.mock';
+import { resendAccountVerificationCodeGeneralError } from '@business/module/errors/user/user';
+
+describe('2-business.useCases.user.resendAccountVerificationCodeUseCase', () => {
+  beforeEach(() => {
+    container.snapshot();
+  });
+
+  afterEach(() => {
+    container.restore();
+  });
+
+  const useCase = container.get(ResendAccountVerificationCodeUseCase);
+  const input: InputResendAccountVerificationCodeDto = {
+    email: 'email@mail.com',
+  };
+
+  it('should is not be able to resend code because exception in findByEmail method', async () => {
+    jest.spyOn(userRepositoryMock, 'findByEmail').mockImplementationOnce(() => {
+      throw new Error('mocked error');
+    });
+
+    const result = await useCase.exec(input);
+
+    expect(result.isRight()).toBeFalsy();
+    expect(result.isLeft()).toBeTruthy();
+    expect(result.value).toEqual(resendAccountVerificationCodeGeneralError);
+  });
+});
