6c8a673c23daff43c2ed5090dc73d096b1f30cb3
Issac Cabral <issaccabrall@gmail.com>
2023-06-08 19:42:18 -0300

    test: should return left if user is not found

--- test/unit/2-business/useCases/user/resendAccountVerificationCodeUseCase.unit.ts
+++ test/unit/2-business/useCases/user/resendAccountVerificationCodeUseCase.unit.ts
@@ -2,7 +2,7 @@ import { container } from '@test/utility/ioc/inversifyConfigTests';
 import { ResendAccountVerificationCodeUseCase } from '@business/useCases/user/resendAccountVerificationCodeUseCase';
 import { InputResendAccountVerificationCodeDto } from '@business/dto/user/resendAccountVerificationCodeDto';
 import { userRepositoryMock } from '@test/utility/mocks/repository/userRepository.mock';
-import { resendAccountVerificationCodeGeneralError } from '@business/module/errors/user/user';
+import { resendAccountVerificationCodeGeneralError, userIsNotFoundError } from '@business/module/errors/user/user';
 
 describe('2-business.useCases.user.resendAccountVerificationCodeUseCase', () => {
   beforeEach(() => {
@@ -38,11 +38,13 @@ describe('2-business.useCases.user.resendAccountVerificationCodeUseCase', () => 
     expect(spy).toHaveBeenCalledWith(input.email);
   });
 
-  // it('should return left if email already exists', async () => {
-  //   const result = await useCase.exec(input);
+  it('should return left if user is not found', async () => {
+    jest.spyOn(userRepositoryMock, 'findByEmail').mockImplementationOnce(async () => null);
 
-  //   expect(result.isRight()).toBeFalsy();
-  //   expect(result.isLeft()).toBeTruthy();
-  //   expect(result.value).toEqual(emailNotAvailableError);
-  // });
+    const result = await useCase.exec(input);
+
+    expect(result.isRight()).toBeFalsy();
+    expect(result.isLeft()).toBeTruthy();
+    expect(result.value).toEqual(userIsNotFoundError);
+  });
 });
