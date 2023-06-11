c39ba43bcb954a03b53e57bc5eb7a04d0fbe5d93
Issac Cabral <issaccabrall@gmail.com>
2023-06-11 13:33:12 -0300

    test: resolve tests

--- test/unit/2-business/useCases/user/createUserUseCase.unit.ts
+++ test/unit/2-business/useCases/user/createUserUseCase.unit.ts
@@ -148,23 +148,29 @@ describe('2-business.useCases.user.createUserUseCase', () => {
     expect(result.value).toEqual(fakeError);
   });
 
-  // it('should calls create user entity with correct values', async () => {
-  //   jest.useFakeTimers().setSystemTime(new Date('2023-01-01T00:00:00.000Z'));
-  //   jest.spyOn(userRepositoryMock, 'findByEmail').mockImplementationOnce(async () => null);
+  it('should calls create user entity with correct values', async () => {
+    const fakeDate = new Date('2023-01-01T00:00:00.000Z');
 
-  //   const spy = jest.spyOn(UserEntity, 'create');
+    jest.useFakeTimers().setSystemTime(fakeDate);
+    jest.spyOn(userRepositoryMock, 'findByEmail').mockImplementationOnce(async () => null);
+    jest.spyOn(dateServiceMock, 'addMinutesToADate').mockImplementationOnce((date: Date, minutes: number) => {
+      date.setMinutes(date.getMinutes() + minutes);
+      return date;
+    });
 
-  //   await useCase.exec(input);
+    const spy = jest.spyOn(UserEntity, 'create');
 
-  //   expect(spy).toHaveBeenCalledWith({
-  //     ...input,
-  //     id: '0c5244eb-d80e-452c-bf99-383236161a51',
-  //     password: 'hash',
-  //     registrationStep: RegistrationStep.PENDING,
-  //     accountVerificationCode: '001',
-  //     accountVerificationCodeExpiresAt: new Date('2023-01-01T00:03:00.000Z'),
-  //   });
-  // });
+    await useCase.exec(input);
+
+    expect(spy).toHaveBeenCalledWith({
+      ...input,
+      id: '0c5244eb-d80e-452c-bf99-383236161a51',
+      password: 'hash',
+      registrationStep: RegistrationStep.PENDING,
+      accountVerificationCode: '001',
+      accountVerificationCodeExpiresAt: new Date('2023-01-01T00:03:00.000Z'),
+    });
+  });
 
   it('should is not be able to create user because exception in create method', async () => {
     jest.spyOn(userRepositoryMock, 'findByEmail').mockImplementationOnce(async () => null);
@@ -180,24 +186,28 @@ describe('2-business.useCases.user.createUserUseCase', () => {
     expect(result.value).toEqual(createUserGeneralError);
   });
 
-  // it('should calls create method with correct values', async () => {
-  //   jest.useFakeTimers().setSystemTime(new Date('2023-01-01T00:00:00.000Z'));
+  it('should calls create method with correct values', async () => {
+    jest.useFakeTimers().setSystemTime(new Date('2023-01-01T00:00:00.000Z'));
 
-  //   jest.spyOn(userRepositoryMock, 'findByEmail').mockImplementationOnce(async () => null);
+    jest.spyOn(userRepositoryMock, 'findByEmail').mockImplementationOnce(async () => null);
+    jest.spyOn(dateServiceMock, 'addMinutesToADate').mockImplementationOnce((date: Date, minutes: number) => {
+      date.setMinutes(date.getMinutes() + minutes);
+      return date;
+    });
 
-  //   const spy = jest.spyOn(userRepositoryMock, 'create');
+    const spy = jest.spyOn(userRepositoryMock, 'create');
 
-  //   await useCase.exec(input);
+    await useCase.exec(input);
 
-  //   expect(spy).toHaveBeenCalledWith({
-  //     ...input,
-  //     id: '0c5244eb-d80e-452c-bf99-383236161a51',
-  //     password: 'hash',
-  //     registrationStep: RegistrationStep.PENDING,
-  //     accountVerificationCode: '001',
-  //     accountVerificationCodeExpiresAt: new Date('2023-01-01T00:03:00.000Z'),
-  //   });
-  // });
+    expect(spy).toHaveBeenCalledWith({
+      ...input,
+      id: '0c5244eb-d80e-452c-bf99-383236161a51',
+      password: 'hash',
+      registrationStep: RegistrationStep.PENDING,
+      accountVerificationCode: '001',
+      accountVerificationCodeExpiresAt: new Date('2023-01-01T00:03:00.000Z'),
+    });
+  });
 
   it('should create user on success', async () => {
     jest.spyOn(userRepositoryMock, 'findByEmail').mockImplementationOnce(async () => null);
