429d701812efa5bf3e0eb5063e53cc63d85b5e85
Issac Cabral <issaccabrall@gmail.com>
2023-07-17 07:50:27 -0300

    test: should calls findById method with correct value

--- test/unit/2-business/useCases/equipmentCategory/updateEquipmentCategoryUseCase.unit.ts
+++ test/unit/2-business/useCases/equipmentCategory/updateEquipmentCategoryUseCase.unit.ts
@@ -38,13 +38,13 @@ describe('2-business.useCases.equipmentCategory.updateEquipmentCategoryUseCase',
     expect(result.value).toEqual(updateEquipmentCategoryGeneralError);
   });
 
-  // it('should calls findByName method with correct value', async () => {
-  //   const spy = jest.spyOn(equipmentCategoryRepositoryMock, 'findByName');
+  it('should calls findById method with correct value', async () => {
+    const spy = jest.spyOn(equipmentCategoryRepositoryMock, 'findById');
 
-  //   await useCase.exec(input);
+    await useCase.exec(input);
 
-  //   expect(spy).toHaveBeenCalledWith(input.name);
-  // });
+    expect(spy).toHaveBeenCalledWith(input.id);
+  });
 
   // it('should return left if name is already in use', async () => {
   //   const result = await useCase.exec(input);
