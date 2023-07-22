526763ab3415e38dfbcb2605de444cbc03191c25
Issac Cabral <issaccabrall@gmail.com>
2023-07-22 17:52:02 -0300

    chore: add binds

--- src/4-framework/ioc/controllersModule.ts
+++ src/4-framework/ioc/controllersModule.ts
@@ -1,10 +1,12 @@
 import { CreateEquimentCategoryController } from '@framework/server/controllers/equipmentCategory/createEquipmentCategoryController';
 import { DeleteEquimentCategoryController } from '@framework/server/controllers/equipmentCategory/deleteEquipmentCategoryController';
 import { ListEquipmentCategoriesController } from '@framework/server/controllers/equipmentCategory/listEquipmentCategoriesController';
+import { UpdateEquipmentCategoryController } from '@framework/server/controllers/equipmentCategory/updateEquipmentCategoryController';
 import { ContainerModule, interfaces } from 'inversify';
 
 export const ControllersModule = new ContainerModule((bind: interfaces.Bind) => {
   bind(CreateEquimentCategoryController).toSelf();
   bind(DeleteEquimentCategoryController).toSelf();
   bind(ListEquipmentCategoriesController).toSelf();
+  bind(UpdateEquipmentCategoryController).toSelf();
 });
