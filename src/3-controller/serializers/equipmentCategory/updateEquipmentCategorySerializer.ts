aeffaeb1e8672d8b4ae5f7020f817e63e43e89b6
Issac Cabral <issaccabrall@gmail.com>
2023-07-22 17:50:42 -0300

    chore: define update equipment category serializer

---
+++ src/3-controller/serializers/equipmentCategory/updateEquipmentCategorySerializer.ts
@@ -0,0 +1,26 @@
+import { IsNotEmpty, IsOptional, IsString, IsUUID, Length } from 'class-validator';
+import { AbstractSerializer } from '../abstractSerializer';
+import { OutputUpdateEquipmentCategoryDto } from '@business/dto/equipmentCategoryDto.ts/updateEquipmentCategoryDto';
+
+class Params extends AbstractSerializer<Params> {
+  @IsString()
+  @IsOptional()
+  @Length(3, 25)
+  name?: string;
+
+  @IsString()
+  @IsOptional()
+  @Length(3, 100)
+  description?: string;
+}
+
+export class InputUpdateEquipmentCategory extends AbstractSerializer<InputUpdateEquipmentCategory> {
+  @IsUUID()
+  @IsNotEmpty()
+  id!: string;
+
+  @IsNotEmpty()
+  params!: Params;
+}
+
+export type OutputUpdateEquipmentCategory = OutputUpdateEquipmentCategoryDto;
