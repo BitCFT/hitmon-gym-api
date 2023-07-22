dbed23bcbfa4348b5b918958bd5693c5178a5f06
Issac Cabral <issaccabrall@gmail.com>
2023-07-22 18:42:24 -0300

    chore: add equipment repository

---
+++ src/2-business/repositories/equipment/iEquipmentRepository.ts
@@ -0,0 +1,29 @@
+import { IEquipmentEntity } from '@domain/entities/equipmentEntity';
+import { PaginationData, PaginationParams } from '@domain/pagination';
+
+export const IEquipmentRepositoryToken = Symbol.for('IEquipmentRepositoryToken');
+
+export type InputCreateEquipment = {
+  id: string;
+  name: string;
+  price: string;
+  categoryId: string;
+};
+
+export type OutputFindByName = IEquipmentEntity | null;
+export type OutputFindById = IEquipmentEntity | null;
+export type InputListAllEquipments = PaginationParams;
+export type OutputListAllEquipments = PaginationData<IEquipmentEntity>;
+export type InputUpdateEquipment = {
+  id: string;
+  params: Partial<Pick<IEquipmentEntity, 'name' | 'price' | 'categoryId'>>;
+};
+
+export interface IEquipmentRepository {
+  create(input: InputCreateEquipment): Promise<IEquipmentEntity>;
+  findByName(name: string): Promise<OutputFindByName>;
+  findById(id: string): Promise<OutputFindById>;
+  listAll(input: InputListAllEquipments): Promise<OutputListAllEquipments>;
+  update(input: InputUpdateEquipment): Promise<IEquipmentEntity>;
+  delete(id: string): Promise<void>;
+}
