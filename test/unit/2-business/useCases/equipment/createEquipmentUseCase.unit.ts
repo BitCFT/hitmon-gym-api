8c92574a73b3577a7f1c67127d8fb073036b2670
Issac Cabral <issaccabrall@gmail.com>
2023-07-22 19:15:31 -0300

    test: should calls findByName method with correct value

--- test/unit/2-business/useCases/equipment/createEquipmentUseCase.unit.ts
+++ test/unit/2-business/useCases/equipment/createEquipmentUseCase.unit.ts
@@ -32,13 +32,13 @@ describe('2-business.useCases.equipment.createEquipmentUseCase', () => {
     expect(result.value).toEqual(createEquipmentGeneralError);
   });
 
-  // it('should calls findByName method with correct value', async () => {
-  //   const spy = jest.spyOn(equipmentRepositoryMock, 'findByName');
+  it('should calls findByName method with correct value', async () => {
+    const spy = jest.spyOn(equipmentRepositoryMock, 'findByName');
 
-  //   await useCase.exec(input);
+    await useCase.exec(input);
 
-  //   expect(spy).toHaveBeenCalledWith(input.name);
-  // });
+    expect(spy).toHaveBeenCalledWith(input.name);
+  });
 
   // it('should return left if name is already in use', async () => {
   //   const result = await useCase.exec(input);
