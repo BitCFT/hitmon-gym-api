0547b49bea7e4c947c4238abfb6a7e3c0e7da564
Issac Cabral <issaccabrall@gmail.com>
2023-07-22 19:12:37 -0300

    chore: create fakes and mocks

--- test/utility/ioc/fakeRepositoriesModule.ts
+++ test/utility/ioc/fakeRepositoriesModule.ts
@@ -6,10 +6,13 @@ import {
   IEquipmentCategoryRepositoryToken,
 } from '@business/repositories/equipmentCategory/iEquipmentCategoryRepository';
 import { equipmentCategoryRepositoryMock } from '../mocks/repository/equipmentCategory.mock';
+import { IEquipmentRepository, IEquipmentRepositoryToken } from '@business/repositories/equipment/iEquipmentRepository';
+import { equipmentRepositoryMock } from '../mocks/repository/equipment.mock';
 
 export const FakeRepositoriesModule = (container: Container) => {
   container.bind<IUserRepository>(IUserRepositoryToken).toConstantValue(userRepositoryMock);
   container
     .bind<IEquipmentCategoryRepository>(IEquipmentCategoryRepositoryToken)
     .toConstantValue(equipmentCategoryRepositoryMock);
+  container.bind<IEquipmentRepository>(IEquipmentRepositoryToken).toConstantValue(equipmentRepositoryMock);
 };
