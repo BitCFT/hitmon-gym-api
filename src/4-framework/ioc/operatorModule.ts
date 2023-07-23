768f5ff4dea5acb6a594b81e630dcfa58a98d4f6
Issac Cabral <issaccabrall@gmail.com>
2023-07-23 10:39:02 -0300

    chore: add operator and serializer

--- src/4-framework/ioc/operatorModule.ts
+++ src/4-framework/ioc/operatorModule.ts
@@ -1,4 +1,5 @@
 import { CreateEquipmentOperator } from '@controller/operators/equipment/createEquipmentOperator';
+import { DeleteEquipmentOperator } from '@controller/operators/equipment/deleteEquipmentOperator';
 import { ListEquipmentsOperator } from '@controller/operators/equipment/listEquipmentsOperator';
 import { CreateEquipmentCategoryOperator } from '@controller/operators/equipmentCategory/createEquipmentCategoryOperator';
 import { DeleteEquipmentCategoryOperator } from '@controller/operators/equipmentCategory/deleteEquipmentCategoryOperator';
@@ -13,4 +14,5 @@ export const OperatorModule = new ContainerModule((bind: interfaces.Bind) => {
   bind(UpdateEquipmentCategoryOperator).toSelf();
   bind(CreateEquipmentOperator).toSelf();
   bind(ListEquipmentsOperator).toSelf();
+  bind(DeleteEquipmentOperator).toSelf();
 });
