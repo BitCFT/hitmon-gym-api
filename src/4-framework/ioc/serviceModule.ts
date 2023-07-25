a764d6aa4381e86598ba93f86999e3b889f6f98b
Issac Cabral <issaccabrall@gmail.com>
2023-07-24 22:18:40 -0300

    fix: bind queue service

--- src/4-framework/ioc/serviceModule.ts
+++ src/4-framework/ioc/serviceModule.ts
@@ -1,11 +1,13 @@
 import { IDateService, IDateServiceToken } from '@business/services/iDateService';
 import { IHashService, IHashServiceToken } from '@business/services/iHashService';
 import { ILoggerService, ILoggerServiceToken } from '@business/services/iLogger';
+import { IQueueService, IQueueServiceToken } from '@business/services/iQueueService';
 import { IRandomCodeService, IRandomCodeServiceToken } from '@business/services/iRandomCodeService';
 import { IUniqueIdentifierService, IUniqueIdentifierServiceToken } from '@business/services/iUniqueIdentifierService';
 import { DateService } from '@framework/services/dateService';
 import { HashService } from '@framework/services/hashService';
 import { LoggerService } from '@framework/services/loggerService';
+import { QueueService } from '@framework/services/queueService';
 import { RandomCodeService } from '@framework/services/randomCodeService';
 import { UniqueIdentifierService } from '@framework/services/uniqueIdentifierService';
 import { ContainerModule, interfaces } from 'inversify';
@@ -16,4 +18,5 @@ export const ServicesModule = new ContainerModule((bind: interfaces.Bind) => {
   bind<IHashService>(IHashServiceToken).to(HashService);
   bind<IRandomCodeService>(IRandomCodeServiceToken).to(RandomCodeService);
   bind<IDateService>(IDateServiceToken).to(DateService);
+  bind<IQueueService>(IQueueServiceToken).to(QueueService);
 });
