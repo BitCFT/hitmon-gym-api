c608519557bff0f6280cfd4dd9ac87f93bd189ed
Issac Cabral <issaccabrall@gmail.com>
2023-07-24 21:34:38 -0300

    feat: implements date service

--- src/4-framework/ioc/serviceModule.ts
+++ src/4-framework/ioc/serviceModule.ts
@@ -1,7 +1,9 @@
+import { IDateService, IDateServiceToken } from '@business/services/iDateService';
 import { IHashService, IHashServiceToken } from '@business/services/iHashService';
 import { ILoggerService, ILoggerServiceToken } from '@business/services/iLogger';
 import { IRandomCodeService, IRandomCodeServiceToken } from '@business/services/iRandomCodeService';
 import { IUniqueIdentifierService, IUniqueIdentifierServiceToken } from '@business/services/iUniqueIdentifierService';
+import { DateService } from '@framework/services/dateService';
 import { HashService } from '@framework/services/hashService';
 import { LoggerService } from '@framework/services/loggerService';
 import { RandomCodeService } from '@framework/services/randomCodeService';
@@ -13,4 +15,5 @@ export const ServicesModule = new ContainerModule((bind: interfaces.Bind) => {
   bind<ILoggerService>(ILoggerServiceToken).to(LoggerService);
   bind<IHashService>(IHashServiceToken).to(HashService);
   bind<IRandomCodeService>(IRandomCodeServiceToken).to(RandomCodeService);
+  bind<IDateService>(IDateServiceToken).to(DateService);
 });
